01a47eb6a8361c52116966605819dc511e675b2c
Evgeny Dudnik <e.dudnik@itq-group.com>
2022-05-16 16:19:12 +0300

    creating keypad

--- src/components/Row.js
+++ src/components/Row.js
@@ -4,7 +4,7 @@ export default function ({ guess, currentGuess }) {
 
   if (guess) {
     return (
-      <div className="row">
+      <div className="row past">
         {guess.map((letter, index) => (
           <div key={index} className={letter.color}>{letter.key}</div>
         ))}
